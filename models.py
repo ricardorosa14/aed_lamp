1d0a147baf7d9a935502d3521550b2d86ee60add
gcgibert <gerard.carramona@gmail.com>
2020-03-25 17:02:40 +0000

    class ColorLamp and class LampArray added.

--- models.py
+++ models.py
@@ -10,3 +10,45 @@ class Lamp:
 
     def set_off(self):
         self.state = False
+
+class ColorLamp(Lamp):
+    def __init__(self):
+        Lamp.__init__(self)
+        self.color = 'white'
+
+    def set_color(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+class LampArray(ColorLamp):
+    def __init__(self):
+        self.lamps = []
+
+    def creatLamp(self):
+        lamp = ColorLamp()
+        self.lamps.append(lamp)
+        return lamp
+
+    def set_all_on(self):
+        for lamp in self.lamps:
+            lamp.set_on()
+    
+    def set_all_off(self):
+        for lamp in self.lamps:
+            lamp.set_off()
+
+    def check_all_on(self):
+        for lamp in self.lamps:
+            if lamp.is_on() == False:
+                return False
+        
+        return True
+
+    def check_all_off(self):
+        for lamp in self.lamps:
+            if lamp.is_on() == True:
+                return False
+        
+        return True
