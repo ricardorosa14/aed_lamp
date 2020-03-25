97bddbb028f1e6ab571a75bcac47a68769962b73
gcgibert <gerard.carramona@gmail.com>
2020-03-25 17:05:18 +0000

    Add tests.

--- test_models.py
+++ test_models.py
@@ -1,13 +1,27 @@
 import unittest
-from models import ColorLamp
+from models import LampArray
 
 class TestColorLamp(unittest.TestCase):
     def setUp(self):
-        self.lamp = ColorLamp()
+        self.controller = LampArray()
 
     def test_set_color(self):
-        self.lamp.set_color('Red')
-        self.assertEqual(self.lamp.get_color(), 'Red')
+        lamp = self.controller.creatLamp()
+        lamp.set_color('Red')
+        self.assertEqual(lamp.get_color(), 'Red')
+
+    def test_set_all_on(self):
+        self.controller.creatLamp()
+        self.controller.creatLamp()
+        self.controller.set_all_on()
+        self.assertTrue(self.controller.check_all_on())
+
+    def test_set_all_off(self):
+        self.controller.creatLamp()
+        self.controller.creatLamp()
+        self.controller.set_all_off()
+        self.assertTrue(self.controller.check_all_off())
+
 
 if __name__ == "__main__":
     unittest.main()
