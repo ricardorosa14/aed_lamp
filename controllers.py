e995a26a61deaf030161750cad47b4d30a904930
gcgibert <gerard.carramona@gmail.com>
2020-04-05 00:28:50 +0100

    Add functions.

--- controllers.py
+++ controllers.py
@@ -6,46 +6,54 @@ class LampController:
     def __init__(self):
         self.lamps = []
 
-    def add_lamp(self, id):
+    def create_lamp(self, id):
         lamp = Lamp()
         dic = {
-            'id' : id,
-            'lamp' : lamp
+            'ID' : id,
+            'lamp' : lamp  
         }
         self.lamps.append(dic)
     
-    def add_ColorLamp(self, id, color):
+    def create_ColorLamp(self, id, color):
         lamp = ColorLamp(color)
         dic = {
-            'id' : id,
+            'ID' : id,
             'lamp' : lamp
         }
         self.lamps.append(dic)
 
-    def add_LampArray(self, id):
+    def create_LampArray(self, id):
         lamp = LampArray()
         dic = {
-            'id' : id,
+            'ID' : id,
             'lamp' : lamp
         }
         self.lamps.append(dic)
+
+    def add_lamp(self, lamp_id, lamp_array_id):
+        for lamp in self.lamps:
+            if lamp['ID'] == lamp_array_id:
+                for new_lamp in self.lamps:
+                    if new_lamp['ID'] == lamp_id:
+                        lamp.append(new_lamp)
 
 
     def set_on(self, id):
         # Change lamp status to ON
         for lamp in self.lamps:
-            if lamp['id'] == id:
-                lamp['lamp'].set_on()
+            if lamp['ID'] == id:
+                lamp['lamp'].set_on() 
 
     def set_off(self, id):
         # Change lamp status to OFF
         for lamp in self.lamps:
-            if lamp['id'] == id:
-                lamp['lamp'].set_off()
+            if lamp['ID'] == id:
+                lamp['lamp'].set_off() 
         
     def get_status(self, id):
         # Returns True if lamp is ON
         for lamp in self.lamps:
-            if lamp['id'] == id:
-                return lamp['lamp'].get_status()
+            if lamp['ID'] == id:
+                return lamp['lamp'].is_on()
+        
         
