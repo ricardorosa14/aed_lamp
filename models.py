6aaeb0191b6db029a2cd5547458145730e541ea7
gcgibert <gerard.carramona@gmail.com>
2020-04-05 00:30:32 +0100

    Add LampArray.

--- models.py
+++ models.py
@@ -12,9 +12,9 @@ class Lamp:
         self.state = False
 
 class ColorLamp(Lamp):
-    def __init__(self):
+    def __init__(self, color):
         Lamp.__init__(self)
-        self.color = "White"
+        self.color = color
 
     def set_color(self, color):
         self.color = color
